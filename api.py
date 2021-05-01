3d7935446d2338c8996a05acace4f38a8a370713
Full-Stack Developer <newemporer0826@gmail.com>
2021-05-01 17:00:23 +0200

    fix 2 message error

--- api.py
+++ api.py
@@ -12,6 +12,6 @@ def SERVER_MESSAGE(sender,message):
     return(received_messages)
 
 if __name__ == "__main__":
-    print(json.loads(SERVER_MESSAGE("123",str("hello"))))
+    print(json.loads(SERVER_MESSAGE("123",str("I am a dealer"))))
 
  
