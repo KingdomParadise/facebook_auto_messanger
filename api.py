a43d307ce2f2f350e52b8bdb54fb10654cac67ea
Full-Stack Developer <ericlee.apollo@gmail.com>
2021-05-01 15:17:17 +0300

    fixed

---
+++ api.py
@@ -0,0 +1,17 @@
+
+import requests,json
+def SERVER_MESSAGE(sender,message):
+    url="http://99.79.146.226:5006/webhooks/rest/webhook"
+    data = json.dumps({
+        "sender": str(sender),
+        "message":str(message)
+    })
+    data_received = requests.post(url, data=data,headers={"Content-Type":"application/json"})
+    received_messages = json.dumps([x['text'] for x in data_received.json()])
+    print(received_messages) 
+    return(received_messages)
+
+if __name__ == "__main__":
+    SERVER_MESSAGE('123','HELLO')
+
+ 
